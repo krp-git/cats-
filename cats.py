9c900b32363b5c3638c97fcc80302d0e1317f265
krp-git <h2rm24@gmail.com>
2026-09-08 22:15:07 +0300

    Add files via upload

---
+++ cats.py
@@ -0,0 +1,31 @@
+class Cat:
+    
+    cat_count = 0
+
+    def __init__(self, name="Кіт", age=1, hunger=50):
+        self.name = name
+        self.age = age
+        self.hunger = hunger
+        print(f"Народилося кошеня на ім'я {self.name}")
+        Cat.cat_count += 1
+
+    def eat(self, food=20):
+        self.hunger -= food
+        if self.hunger < 0:
+            self.hunger = 0
+        print(f"{self.name} поїв(-ла). Голод: {self.hunger}")
+
+    def __str__(self):
+        return f"Кіт {self.name}: вік {self.age}, голод {self.hunger}"
+
+
+first_cat = Cat()
+second_cat = Cat(name="Барсик", age=3)
+
+print(first_cat)
+print(second_cat)
+
+first_cat.eat()
+
+print(first_cat)
+print("Всього котів:", Cat.cat_count)
